0a7afe54184bab20299316e360a8fda461722af0
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2023-03-04 21:50:53 -0300

    Backend configurado

---
+++ backend/src/main/java/br/com/chadschoperia/ChadsChoperiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.chadschoperia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadsChoperiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChadsChoperiaApplication.class, args);
+    }
+
+}
