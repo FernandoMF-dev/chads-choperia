ce1332d0b6e499ffe237e78ccafba6f3f42ad9f8
FernandoMF <fernandomartinsferreira70mil@gmail.com>
2023-03-13 14:44:10 -0300

    Criando modulo 'SelfserviceModule'

---
+++ frontend/src/app/modules/selfservice/selfservice.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SelfserviceRoutingModule } from './selfservice-routing.module';
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		SelfserviceRoutingModule
+	]
+})
+export class SelfserviceModule {
+}
