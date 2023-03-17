ee50fc870f937f352aecdbe4ec82dc55e5afa71a
FernandoMF <fernandomartinsferreira70mil@gmail.com>
2023-03-17 09:53:52 -0300

    Implementando listagem de notificação de restock no componente 'RestockNotificationListComponent'

--- frontend/src/app/modules/selfservice/pages/restock-notification-list/restock-notification-list.component.ts
+++ frontend/src/app/modules/selfservice/pages/restock-notification-list/restock-notification-list.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { UtilsService } from '../../../../services/utils.service';
+import { RestockNotification } from '../../../user/models/restock-notification.model';
+import { RestockNotificationService } from '../../services/restock-notification.service';
 
 @Component({
 	selector: 'app-restock-notification-list',
 	templateUrl: './restock-notification-list.component.html',
 	styleUrls: ['./restock-notification-list.component.scss']
 })
-export class RestockNotificationListComponent {
+export class RestockNotificationListComponent implements OnInit {
 
+	notifications: RestockNotification[] = [];
+	confirmRestock: number | null = null;
+	stopListenerDocumentClick: boolean = false;
+
+
+	_isLoading = false;
+
+	get isLoading(): boolean {
+		return this._isLoading;
+	}
+
+	set isLoading(value: boolean) {
+		this._isLoading = value;
+	}
+
+	constructor(
+		private restockNotificationService: RestockNotificationService,
+		private utilsService: UtilsService
+	) {
+	}
+
+	ngOnInit(): void {
+		this.findAllNotifications();
+	}
+
+	@HostListener('document:click')
+	onDocumentClick() {
+		if (this.stopListenerDocumentClick) {
+			this.stopListenerDocumentClick = false;
+		} else {
+			this.confirmRestock = null;
+		}
+	}
+
+	findAllNotifications(): void {
+		this.isLoading = true;
+		this.restockNotificationService.findAll()
+			.pipe(finalize(() => this.isLoading = false))
+			.subscribe({
+				next: (res) => this.notifications = res,
+				error: () => this.utilsService.showErrorMessage('Erro ao carregar dados')
+			});
+	}
+
+	onRestockClick(notificationId: number) {
+		this.stopListenerDocumentClick = true;
+		this.confirmRestock = notificationId;
+	}
+
+	onConfirmRestock(): void {
+		const notificationId: number = this.confirmRestock!;
+
+		this.isLoading = true;
+		this.restockNotificationService.replaceStock(notificationId)
+			.pipe(finalize(() => this.isLoading = false))
+			.subscribe({
+				next: () => this.notifications = this.notifications.filter(value => value.id !== notificationId),
+				error: () => this.utilsService.showErrorMessage()
+			});
+	}
 }
--- frontend/src/app/modules/selfservice/selfservice.module.ts
+++ frontend/src/app/modules/selfservice/selfservice.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { SkeletonModule } from 'primeng/skeleton';
+import { TableModule } from 'primeng/table';
 import { RestockNotificationListComponent } from './pages/restock-notification-list/restock-notification-list.component';
 
 import { SelfserviceRoutingModule } from './selfservice-routing.module';
+import { RestockNotificationService } from './services/restock-notification.service';
 
 
 @NgModule({
@@ -10,8 +14,14 @@ import { SelfserviceRoutingModule } from './selfservice-routing.module';
 		RestockNotificationListComponent
 	],
 	imports: [
+		SelfserviceRoutingModule,
 		CommonModule,
-		SelfserviceRoutingModule
+		TableModule,
+		SkeletonModule,
+		ButtonModule
+	],
+	providers: [
+		RestockNotificationService
 	]
 })
 export class SelfserviceModule {
