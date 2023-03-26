4504178e3cbc77c3899ab8d03cd81e38ab71b4ce
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-03-26 15:05:17 -0300

    Criando ClientModule

---
+++ frontend/src/app/modules/client/client.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ClientRoutingModule } from './client-routing.module';
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		ClientRoutingModule
+	]
+})
+export class ClientModule {
+}
