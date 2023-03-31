8badda96e84e40d1dc4d02d2162cd731d565e555
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-03-31 16:18:48 -0300

    Implementando tratamento de erros de 'ResponseEntity'

--- backend/src/main/java/br/com/chadschoperia/service/exception/ValidationHandler.java
+++ backend/src/main/java/br/com/chadschoperia/service/exception/ValidationHandler.java
@@ -1,9 +1,9 @@
 package br.com.chadschoperia.service.exception;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
@@ -11,10 +11,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
@@ -22,13 +24,16 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @ControllerAdvice
 public class ValidationHandler extends ResponseEntityExceptionHandler {
 
-	@Autowired
-	private MessageSource messageSource;
+	private final static String DEFAULT_ERROR_MESSAGE = "default.error.bad_request";
+
+	private final MessageSource messageSource;
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -39,8 +44,24 @@ public class ValidationHandler extends ResponseEntityExceptionHandler {
 				.map(DefaultMessageSourceResolvable::getDefaultMessage)
 				.collect(Collectors.toList());
 
-		return getExceptionResponseEntity(HttpStatus.BAD_REQUEST, request, errors);
+		return getExceptionResponseEntity(HttpStatus.resolve(status.value()), request, errors);
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleErrorResponseException(ErrorResponseException ex,
+																  HttpHeaders headers,
+																  HttpStatusCode status,
+																  WebRequest request) {
+		List<String> errors = List.of(DEFAULT_ERROR_MESSAGE);
+
+		if (ex instanceof ResponseStatusException) {
+			errors = List.of(Objects.requireNonNull(((ResponseStatusException) ex).getReason()));
+		}
+
+		return getExceptionResponseEntity(HttpStatus.resolve(status.value()), request, errors);
+	}
+
+
 
 	@ExceptionHandler({ConstraintViolationException.class})
 	public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception, WebRequest request) {
