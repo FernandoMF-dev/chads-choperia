cf354f258199f5a8ef440a318d1c393c6527f020
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-04-01 16:50:16 -0300

    Implementando busca de ClientCard por RFID sem tratamentos para teste

--- backend/src/main/java/br/com/chadschoperia/repository/ClientCardRepository.java
+++ backend/src/main/java/br/com/chadschoperia/repository/ClientCardRepository.java
@@ -1,9 +1,19 @@
 package br.com.chadschoperia.repository;
 
 import br.com.chadschoperia.domain.entities.ClientCard;
+import br.com.chadschoperia.domain.enums.ClientCardStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientCardRepository extends JpaRepository<ClientCard, Long> {
+
+	@Query("SELECT cc " +
+			" FROM ClientCard cc " +
+			" WHERE cc.rfid = :rfid " +
+			" AND cc.status = :status ")
+	ClientCard findByRfid(@Param("rfid") Long rfid, @Param("status") ClientCardStatusEnum status);
+
 }
--- backend/src/main/java/br/com/chadschoperia/service/ClientCardService.java
+++ backend/src/main/java/br/com/chadschoperia/service/ClientCardService.java
@@ -1,6 +1,9 @@
 package br.com.chadschoperia.service;
 
+import br.com.chadschoperia.domain.entities.ClientCard;
+import br.com.chadschoperia.domain.enums.ClientCardStatusEnum;
 import br.com.chadschoperia.repository.ClientCardRepository;
+import br.com.chadschoperia.service.dto.ClientCardDto;
 import br.com.chadschoperia.service.mapper.ClientCardMapper;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +16,10 @@ public class ClientCardService {
 
 	private final ClientCardRepository repository;
 	private final ClientCardMapper mapper;
+
+	public ClientCardDto findOpenByRfid(Long rfid) {
+		ClientCard entity = repository.findByRfid(rfid, ClientCardStatusEnum.OPEN);
+		return mapper.toDto(entity);
+	}
 
 }
