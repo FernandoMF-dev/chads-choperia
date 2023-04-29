a3e121367788c00177aeeba4b2c85349cdc08883
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-04-29 15:43:45 -0300

    Adicionando uma mensagem decente na descrição de gasto de cliente no self-service

--- backend/src/main/java/br/com/chadschoperia/service/SelfservicePurchaseService.java
+++ backend/src/main/java/br/com/chadschoperia/service/SelfservicePurchaseService.java
@@ -1,33 +1,44 @@
 package br.com.chadschoperia.service;
 
 import br.com.chadschoperia.domain.entities.SelfservicePurchase;
+import br.com.chadschoperia.exceptions.EntityNotFoundException;
 import br.com.chadschoperia.repository.SelfservicePurchaseRepository;
+import br.com.chadschoperia.service.dto.ClientCardDto;
 import br.com.chadschoperia.service.dto.SelfservicePurchaseDto;
 import br.com.chadschoperia.service.events.AddClientCardExpenseEvent;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class SelfservicePurchaseService {
 
-	private final ApplicationEventPublisher applicationEventPublisher;
-
 	private final SelfservicePurchaseRepository repository;
 
 	private final ClientCardService clientCardService;
-	public static final String SELF_SERVICE_DESCRIPTION = "Self-service";
 
+	private final MessageSource messageSource;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	public SelfservicePurchaseDto insertExpense(SelfservicePurchaseDto dto) {
-		double value = dto.getWeight() * getPricePerKg();
-		applicationEventPublisher.publishEvent(new AddClientCardExpenseEvent(clientCardService.findOpenByRfid(dto.getCardId().toString()).getId(), value, SELF_SERVICE_DESCRIPTION));
-		return dto;
+		try {
+			ClientCardDto card = clientCardService.findOpenByRfid(dto.getCardId().toString());
+			double value = dto.getWeight() * getPricePerKg();
+			String formatedValue = String.format("%02.3f", value);
+			String description = messageSource.getMessage("selfservice.purchase.description", new String[]{formatedValue}, Locale.getDefault());
+			applicationEventPublisher.publishEvent(new AddClientCardExpenseEvent(card.getId(), value, description));
+			return dto;
+		} catch (EntityNotFoundException ex) {
+			throw new EntityNotFoundException(HttpStatus.BAD_REQUEST, ex.getReason());
+		}
 	}
 
 	public void insertPricePerKg(double pricePerKg) {
