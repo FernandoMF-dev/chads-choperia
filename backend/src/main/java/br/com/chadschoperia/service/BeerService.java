507f48ac49dd54e8cc9c64fc25a38e4c03e06115
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-04-30 15:56:29 -0300

    Salvando histórico das alterações de chope

--- backend/src/main/java/br/com/chadschoperia/service/BeerService.java
+++ backend/src/main/java/br/com/chadschoperia/service/BeerService.java
@@ -1,6 +1,7 @@
 package br.com.chadschoperia.service;
 
 import br.com.chadschoperia.domain.entities.Beer;
+import br.com.chadschoperia.domain.enums.HistoricBeerActionEnum;
 import br.com.chadschoperia.exceptions.BusinessException;
 import br.com.chadschoperia.exceptions.EntityNotFoundException;
 import br.com.chadschoperia.repository.BeerRepository;
@@ -10,6 +11,8 @@ import br.com.chadschoperia.service.dto.PourBeerDTO;
 import br.com.chadschoperia.service.dto.ProductStockDto;
 import br.com.chadschoperia.service.dto.ViewBeerDto;
 import br.com.chadschoperia.service.events.AddClientCardExpenseEvent;
+import br.com.chadschoperia.service.events.AddHistoricBeerEvent;
+import br.com.chadschoperia.service.events.AddListHistoricBeerEvent;
 import br.com.chadschoperia.service.mapper.BeerMapper;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +21,10 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -65,23 +69,35 @@ public class BeerService {
 
 	public BeerDto create(BeerDto beerDto) {
 		validatePrices(beerDto);
+
 		beerDto.setId(null);
 		beerDto.setStock(0D);
-		return saveDto(beerDto);
+		beerDto = saveDto(beerDto);
+
+		publishHistoric(beerDto.getId(), HistoricBeerActionEnum.CREATE);
+
+		return beerDto;
 	}
 
 	public BeerDto update(BeerDto beerDto) {
-		BeerDto originalDto = findDtoById(beerDto.getId());
 		validatePrices(beerDto);
+
+		BeerDto originalDto = findDtoById(beerDto.getId());
 		beerDto.setStock(originalDto.getStock());
-		return saveDto(beerDto);
+		beerDto = saveDto(beerDto);
+
+		publishHistoric(beerDto.getId(), HistoricBeerActionEnum.UPDATE);
+
+		return beerDto;
 	}
 
 	public List<BeerDto> restock(List<ProductStockDto> dtos) {
 		List<Long> productIds = dtos.stream().map(ProductStockDto::getProductId).collect(Collectors.toList());
 		List<Beer> beers = beerRepository.findAllById(productIds);
+		List<Double> finalAmounts = new ArrayList<>();
 
-		beers.forEach(product -> addStockFromSource(product, dtos));
+		beers.forEach(product -> finalAmounts.add(addStockFromSource(product, dtos)));
+		publishListHistoric(HistoricBeerActionEnum.RESTOCK, productIds, finalAmounts);
 
 		return saveDto(beers);
 	}
@@ -89,6 +105,7 @@ public class BeerService {
 	public void pour(PourBeerDTO dto) {
 		BeerDto beer = findDtoById(dto.getBeer());
 		publishPourExpense(dto, beer);
+		publishHistoric(beer.getId(), HistoricBeerActionEnum.POUR, POUR_QUANTITY);
 		beer.subtractStock(POUR_QUANTITY);
 		saveDto(beer);
 	}
@@ -96,15 +113,19 @@ public class BeerService {
 	public void deleteById(Long idBeer) {
 		Beer entity = findEntityById(idBeer);
 		entity.setDeleted(Boolean.TRUE);
+		publishHistoric(entity.getId(), HistoricBeerActionEnum.DELETE);
 		saveEntity(entity);
 	}
 
-	private void addStockFromSource(Beer target, List<ProductStockDto> source) {
-		Optional<ProductStockDto> stockDto = source.stream().filter(dto -> dto.getProductId().equals(target.getId())).findAny();
-		if (stockDto.isEmpty()) {
-			throw new EntityNotFoundException(HttpStatus.BAD_REQUEST, "beer.not_found.restock");
-		}
-		target.addStock(stockDto.get().getAmount() * STOCK_BASE_MULTIPLIER);
+	private double addStockFromSource(Beer target, List<ProductStockDto> source) {
+		ProductStockDto stockDto = source.stream()
+				.filter(dto -> Objects.equals(dto.getProductId(), target.getId()))
+				.findAny()
+				.orElseThrow(() -> new EntityNotFoundException(HttpStatus.BAD_REQUEST, "beer.not_found.restock"));
+
+		double amount = stockDto.getAmount() * STOCK_BASE_MULTIPLIER;
+		target.addStock(amount);
+		return amount;
 	}
 
 	private void publishPourExpense(PourBeerDTO dto, BeerDto beer) {
@@ -115,6 +136,24 @@ public class BeerService {
 		} catch (EntityNotFoundException ex) {
 			throw new EntityNotFoundException(HttpStatus.BAD_REQUEST, ex.getReason());
 		}
+	}
+
+	private void publishHistoric(Long beerId, HistoricBeerActionEnum action) {
+		this.publishHistoric(beerId, action, null);
+	}
+
+	private void publishHistoric(Long beerId, HistoricBeerActionEnum action, Double stock) {
+		applicationEventPublisher.publishEvent(new AddHistoricBeerEvent(beerId, action, stock, null));
+	}
+
+	private void publishListHistoric(HistoricBeerActionEnum action, List<Long> beerId, List<Double> stock) {
+		List<String> descriptions = new ArrayList<>();
+
+		for (int i = 0; i < beerId.size(); i++) {
+			descriptions.add(null);
+		}
+
+		applicationEventPublisher.publishEvent(new AddListHistoricBeerEvent(action, beerId, stock, descriptions));
 	}
 
 	private BeerDto saveDto(BeerDto beer) {
