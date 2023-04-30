203a99b41662edd4eff0c4581747ad81bcf23836
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-04-30 14:19:36 -0300

    Movendo arquivo de configuração de mensagens

--- backend/src/main/java/br/com/chadschoperia/configuration/MessagePropertiesConfig.java
+++ backend/src/main/java/br/com/chadschoperia/configuration/MessagePropertiesConfig.java
@@ -12,7 +12,7 @@ public class MessagePropertiesConfig {
 	@Bean
 	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:/localization/messages");
+		messageSource.setBasename("classpath:/messages/messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		return messageSource;
 	}
