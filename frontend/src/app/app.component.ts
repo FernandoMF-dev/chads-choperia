c94d22b90bcd7c1eaea282754a1fa9e740423607
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-04-30 22:13:46 -0300

    Traduzindo parte da configuração do PrimeNg

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,5 +12,20 @@ export class AppComponent implements OnInit {
 
 	ngOnInit() {
 		this.primengConfig.ripple = true;
+		this.setPrimengTranslation();
+	}
+
+	private setPrimengTranslation(): void {
+		this.primengConfig.setTranslation({
+			firstDayOfWeek: 1,
+			dayNames: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'],
+			dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+			dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+			monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+			monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+			today: 'Hoje',
+			clear: 'Limpar',
+			dateFormat: 'dd/mm/yy'
+		});
 	}
 }
