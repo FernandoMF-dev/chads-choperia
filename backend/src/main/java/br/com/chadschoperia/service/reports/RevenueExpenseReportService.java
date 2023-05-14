629b3737d7225922dadd3ed6000bb48c486157dc
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-05-14 16:06:30 -0300

    Adicionando validação de filtro para requisição de relatório de "receita x despesa"

--- backend/src/main/java/br/com/chadschoperia/service/reports/RevenueExpenseReportService.java
+++ backend/src/main/java/br/com/chadschoperia/service/reports/RevenueExpenseReportService.java
@@ -2,12 +2,14 @@ package br.com.chadschoperia.service.reports;
 
 import br.com.chadschoperia.repository.reports.RevenueExpenseReportRepository;
 import br.com.chadschoperia.service.reports.dto.RevenueExpenseReportDto;
+import br.com.chadschoperia.service.reports.exceptions.InvalidFilterException;
 import br.com.chadschoperia.service.reports.filters.RevenueExpenseReportFilterDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
@@ -16,6 +18,13 @@ public class RevenueExpenseReportService {
 	private final RevenueExpenseReportRepository revenueExpenseReportRepository;
 
 	public List<RevenueExpenseReportDto> getRevenueExpenseOverTime(RevenueExpenseReportFilterDto filter) {
+		validateStockOverTimeFilter(filter);
 		return revenueExpenseReportRepository.getRevenueExpenseOverTime(filter);
+	}
+
+	private void validateStockOverTimeFilter(RevenueExpenseReportFilterDto filter) {
+		if (Objects.nonNull(filter.getMinDate()) && Objects.nonNull(filter.getMaxDate()) && filter.getMaxDate().isBefore(filter.getMinDate())) {
+			throw new InvalidFilterException("report.filter.max_date.less_than.min_date");
+		}
 	}
 }
