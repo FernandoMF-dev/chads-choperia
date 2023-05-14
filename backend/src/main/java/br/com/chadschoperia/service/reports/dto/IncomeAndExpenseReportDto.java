2f6fad905d317e2114631476884edbec0137150f
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-05-14 16:29:44 -0300

    Melhorando DTO de relatório de receita e despesas

--- backend/src/main/java/br/com/chadschoperia/service/reports/dto/IncomeAndExpenseReportDto.java
+++
@@ -1,9 +0,0 @@
-package br.com.chadschoperia.service.reports.dto;
-
-import java.time.LocalDateTime;
-
-public class IncomeAndExpenseReportDto extends BaseReportDto {
-	public IncomeAndExpenseReportDto(String description, Double value, LocalDateTime dateTime) {
-		super(description, value, dateTime);
-	}
-}
