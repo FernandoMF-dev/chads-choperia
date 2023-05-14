82057f40420c2becf4dcc2569fc635c5cf31aed6
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-05-14 16:53:20 -0300

    Adicionando 'ClientExpensesReport' no frontend

--- frontend/src/app/modules/report/models/client-expenses.report.ts
+++ frontend/src/app/modules/report/models/client-expenses.report.ts
@@ -1,7 +1,19 @@
+import { ClientCardStatusEnum } from '../../card/enums/client-card-status.enum';
 import { BaseReport } from './base.report';
 
 export class ClientExpensesReport extends BaseReport {
-	constructor(description: string, value: number, dateTime: Date) {
+	constructor(
+		public clientId: number,
+		public clientName: string,
+		public clientTelephone: string,
+		public clientEmail: string,
+		public clientCardId: number,
+		public clientCardStatus: ClientCardStatusEnum,
+		public clientCardrfid: string,
+		description: string,
+		value: number,
+		dateTime: Date
+	) {
 		super(description, value, dateTime);
 	}
 }
