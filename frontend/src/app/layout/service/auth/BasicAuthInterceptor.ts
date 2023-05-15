541f2a601b3ec0934ca5d18049e0f21c1a2ac002
lucas.rocha <lucas.rocha@basis.com.br>
2023-05-15 12:39:43 -0300

    fix interceptor

--- frontend/src/app/layout/service/auth/BasicAuthInterceptor.ts
+++ frontend/src/app/layout/service/auth/BasicAuthInterceptor.ts
@@ -7,20 +7,19 @@ import { ActiveUserService } from './ActiveUserService';
 @Injectable()
 export class BasicAuthInterceptor implements HttpInterceptor {
 	constructor(
-		// private authenticationService: AuthenticationService
 	) { }
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		const user = ActiveUserService.getInstance().getUser();
 		const basicAuthHeader = 'Basic ' + btoa(`${user?.username}:${user?.password}`);
-
-
-		request = request.clone({
-			setHeaders: {
-				Authorization: basicAuthHeader,
-				'Referrer-Policy': "unsafe-url"
-			}
-		});
+		if(!!user?.username){
+			request = request.clone({
+				setHeaders: {
+					Authorization: basicAuthHeader,
+					'Referrer-Policy': "unsafe-url"
+				}
+			});
+		}
 
 		return next.handle(request);
 	}
