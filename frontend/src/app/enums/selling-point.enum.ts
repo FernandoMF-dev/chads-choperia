3ce978bd257edcd8bd354a4c77c000fcf7f384e7
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-05-20 16:07:25 -0300

    Aprimorando estilização de telas de relatório e adicionando campo de "TOTAL" no relatório de receita

---
+++ frontend/src/app/enums/selling-point.enum.ts
@@ -0,0 +1,6 @@
+export class SellingPointEnum {
+	public static readonly BEER = 'BEER';
+	public static readonly SELF_SERVICE = 'SELF_SERVICE';
+	public static readonly KITCHEN_PRODUCT = 'KITCHEN_PRODUCT';
+	public static readonly MISCELLANEA = 'MISCELLANEA';
+}
--- frontend/src/app/modules/card/models/client-card-expense.model.ts
+++ frontend/src/app/modules/card/models/client-card-expense.model.ts
@@ -1,7 +1,10 @@
+import { SellingPointEnum } from '../../../enums/selling-point.enum';
+
 export class ClientCardExpense {
 	public id?: number;
 	public idCard?: number;
 	public value?: number;
 	public description?: string;
 	public dateTime?: Date;
+	public sellingPoint?: SellingPointEnum;
 }
