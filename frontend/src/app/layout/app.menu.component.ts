cbad68a649e870a65fbbb80d1eb89a0a266cf9ef
Fernando Martins Ferreira <fmartinsf@minsait.com>
2023-05-28 14:58:30 -0300

    Mapeando menus por roles

--- frontend/src/app/layout/app.menu.component.ts
+++ frontend/src/app/layout/app.menu.component.ts
@@ -13,130 +13,157 @@ export class AppMenuComponent implements OnInit {
 
 	model: AppMenuitemType[] = [];
 	private userService: ActiveUserService;
+	private mappedMenus: Map<RolesUtil, AppMenuitemType[]>;
 
 	constructor(public layoutService: LayoutService) {
 		this.userService = ActiveUserService.getInstance();
+		this.mappedMenus = this.initiateMappedMenus();
 	}
 
-	ngOnInit() {
+	ngOnInit(): void {
 		const user = this.userService.getUser();
 		const isLogged = this.userService.isLogged();
+		this.model = [];
+
 		if (isLogged) {
-			this.model = [];
-			const notFound: number = -1;
-			if (user?.roleNames?.indexOf(RolesUtil.ADMIN) != notFound) {
-				this.model.push(this.getAdminMenu());
-			}
-			if (user?.roleNames?.indexOf(RolesUtil.COOK) != notFound) {
-				this.model.push(this.getCookMenu());
-			}
-			if (user?.roleNames?.indexOf(RolesUtil.COSTUMER_MONITOR) != notFound) {
-				this.model.push(this.getCostumerMonitorMenu());
-			}
-			if (user?.roleNames?.indexOf(RolesUtil.CASHIER) != notFound) {
-				this.model.push(this.getCashierMenu());
-			}
-			if (user?.roleNames?.indexOf(RolesUtil.STOCK_MONITOR) != notFound) {
-				this.model.push(this.getStockManagerMenu());
-			}
+			this.mappedMenus.forEach((menu, key) => {
+				if (user?.roleNames?.some(role => role === key)) {
+					this.model.push(...menu);
+				}
+			});
 		} else {
-			this.model.push(this.getCostumerMenu());
+			this.model.push(...this.getPublicMenu());
 		}
 	}
 
-	private getCostumerMenu(): AppMenuitemType {
-		return {
-			label: 'Área do cliente',
-			items: [
-				{ label: 'Chopes', material: 'sports_bar', routerLink: [RouteLinkUtils.BEER, RouteLinkUtils.EXIT] },
-				{
-					label: 'Self-Service',
-					material: 'restaurant',
-					routerLink: [RouteLinkUtils.SELF_SERVICE, RouteLinkUtils.FOOD_WEIGHING]
-				}
-			]
-		};
+	private initiateMappedMenus(): Map<RolesUtil, AppMenuitemType[]> {
+		return new Map<RolesUtil, AppMenuitemType[]>([
+			[RolesUtil.ADMIN, [...this.getAdminMenu()]],
+			[RolesUtil.CASHIER, [...this.getCashierMenu()]],
+			[RolesUtil.COSTUMER, []],
+			[RolesUtil.COOK, [...this.getCookMenu()]],
+			[RolesUtil.COSTUMER_MONITOR, [...this.getCostumerMonitorMenu()]],
+			[RolesUtil.FOOD_MONITOR, [...this.getFoodMonitorMenu()]],
+			[RolesUtil.STOCK_MONITOR, [...this.getStockManagerMenu()]]
+		]);
 	}
 
-	private getStockManagerMenu(): AppMenuitemType {
-		return {
-			label: 'Menu do Fiscal de Estoque',
-			items: [
-				{
-					label: 'Entrada de Estoque',
-					icon: 'pi pi-fw pi-arrow-up-right',
-					routerLink: [RouteLinkUtils.PRODUCT, RouteLinkUtils.ENTRANCE]
-				},
-				{
-					label: 'Entrada de Chope',
-					icon: 'pi pi-fw pi-arrow-up-right',
-					routerLink: [RouteLinkUtils.BEER, RouteLinkUtils.ENTRANCE]
-				}
-			]
-		};
+	private getPublicMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Área do cliente',
+				items: [
+					{ label: 'Chopes', material: 'sports_bar', routerLink: [RouteLinkUtils.BEER, RouteLinkUtils.EXIT] },
+					{
+						label: 'Self-Service',
+						material: 'restaurant',
+						routerLink: [RouteLinkUtils.SELF_SERVICE, RouteLinkUtils.FOOD_WEIGHING]
+					}
+				]
+			}
+		];
 	}
 
-	private getCookMenu(): AppMenuitemType {
-		return {
-			label: 'Menu do Cozinheiro',
-			items: [
-				{
-					label: 'Saída de Estoque',
-					icon: 'pi pi-fw pi-arrow-down-right',
-					routerLink: [RouteLinkUtils.PRODUCT, RouteLinkUtils.EXIT]
-				},
-				{ label: 'Self Service', icon: 'pi pi-fw pi-download', routerLink: [RouteLinkUtils.SELF_SERVICE] }
-			]
-		};
+	private getAdminMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Registros Administrativos',
+				items: [
+					{ label: 'Usuários', icon: 'pi pi-fw pi-user', routerLink: [RouteLinkUtils.USER] },
+					{ label: 'Produtos', icon: 'pi pi-fw pi-box', routerLink: [RouteLinkUtils.PRODUCT] },
+					{ label: 'Chopes', material: 'sports_bar', routerLink: [RouteLinkUtils.BEER] },
+					{
+						label: 'Configurações do Self-Service',
+						material: 'restaurant_menu',
+						routerLink: [RouteLinkUtils.SELF_SERVICE, RouteLinkUtils.SETTING]
+					},
+					{ label: 'Relatórios', material: 'view_list', routerLink: [RouteLinkUtils.REPORT] },
+					{ label: 'E-mail', material: 'email', routerLink: [RouteLinkUtils.EMAIL] }
+				]
+			}
+		];
 	}
 
-	private getCashierMenu(): AppMenuitemType {
-		return {
-			label: 'Menu do Caixa',
-			items: [
-				{
-					label: 'Fechar Conta',
-					material: 'point_of_sale',
-					routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.PAYMENT]
-				}
-			]
-		};
+	private getCashierMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Menu do Caixa',
+				items: [
+					{
+						label: 'Fechar Conta',
+						material: 'point_of_sale',
+						routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.PAYMENT]
+					}
+				]
+			}
+		];
 	}
 
-	private getCostumerMonitorMenu(): AppMenuitemType {
-		return {
-			label: 'Menu do Fiscal de Entrada',
-			items: [
-				{ label: 'Clientes', icon: 'pi pi-fw pi-id-card', routerLink: [RouteLinkUtils.CLIENTE] },
-				{
-					label: 'Vincular Cartão',
-					material: 'add_card',
-					routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.ENTRANCE]
-				},
-				{
-					label: 'Desvincular Cartão',
-					material: 'credit_card_off',
-					routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.EXIT]
-				}
-			]
-		};
+	private getCookMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Menu do Cozinheiro',
+				items: [
+					{
+						label: 'Saída de Estoque',
+						icon: 'pi pi-fw pi-arrow-down-right',
+						routerLink: [RouteLinkUtils.PRODUCT, RouteLinkUtils.EXIT]
+					},
+					{ label: 'Self Service', icon: 'pi pi-fw pi-download', routerLink: [RouteLinkUtils.SELF_SERVICE] }
+				]
+			}
+		];
 	}
 
-	private getAdminMenu(): AppMenuitemType {
-		return {
-			label: 'Registros Administrativos',
-			items: [
-				{ label: 'Usuários', icon: 'pi pi-fw pi-user', routerLink: [RouteLinkUtils.USER] },
-				{ label: 'Produtos', icon: 'pi pi-fw pi-box', routerLink: [RouteLinkUtils.PRODUCT] },
-				{ label: 'Chopes', material: 'sports_bar', routerLink: [RouteLinkUtils.BEER] },
-				{
-					label: 'Configurações do Self-Service',
-					material: 'restaurant_menu',
-					routerLink: [RouteLinkUtils.SELF_SERVICE, RouteLinkUtils.SETTING]
-				},
-				{ label: 'Relatórios', material: 'view_list', routerLink: [RouteLinkUtils.REPORT] },
-				{ label: 'E-mail', material: 'email', routerLink: [RouteLinkUtils.EMAIL] }
-			]
-		};
+	private getCostumerMonitorMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Menu do Fiscal de Entrada',
+				items: [
+					{ label: 'Clientes', icon: 'pi pi-fw pi-id-card', routerLink: [RouteLinkUtils.CLIENTE] },
+					{
+						label: 'Vincular Cartão',
+						material: 'add_card',
+						routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.ENTRANCE]
+					},
+					{
+						label: 'Desvincular Cartão',
+						material: 'credit_card_off',
+						routerLink: [RouteLinkUtils.CARD, RouteLinkUtils.CLIENTE, RouteLinkUtils.EXIT]
+					}
+				]
+			}
+		];
+	}
+
+	private getFoodMonitorMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Menu do Fiscal de Self-service',
+				items: [
+					{ label: 'Notificar falta', icon: 'pi pi-fw pi-download', routerLink: [RouteLinkUtils.SELF_SERVICE] }
+				]
+			}
+		];
+	}
+
+	private getStockManagerMenu(): AppMenuitemType[] {
+		return [
+			{
+				label: 'Menu do Fiscal de Estoque',
+				items: [
+					{
+						label: 'Entrada de Estoque',
+						icon: 'pi pi-fw pi-arrow-up-right',
+						routerLink: [RouteLinkUtils.PRODUCT, RouteLinkUtils.ENTRANCE]
+					},
+					{
+						label: 'Entrada de Chope',
+						icon: 'pi pi-fw pi-arrow-up-right',
+						routerLink: [RouteLinkUtils.BEER, RouteLinkUtils.ENTRANCE]
+					}
+				]
+			}
+		];
 	}
 }
