70cbf05e2501594a2fb576499eca45625af5c090
Álisson Melquíades <armelquiades2000@gmail.com>
2023-06-12 19:05:14 -0300

    Implementando integração com a balança

--- frontend/src/app/modules/selfservice/pages/food-weighing/food-weighing.component.ts
+++ frontend/src/app/modules/selfservice/pages/food-weighing/food-weighing.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { UtilsService } from "src/app/services/utils.service";
 import { foodWeighingService } from "../../services/food-weighing.service";
-
+import { ScaleService } from "src/app/services/scale.service";
 @Component({
 	selector: "app-food-weighing",
 	templateUrl: "./food-weighing.component.html",
@@ -11,10 +11,20 @@ import { foodWeighingService } from "../../services/food-weighing.service";
 export class FoodWeighingComponent {
 	form: FormGroup;
 
-	constructor(private utilsService: UtilsService, private foodWeighingService: foodWeighingService) {
+	constructor(private utilsService: UtilsService, private foodWeighingService: foodWeighingService, private scaleService: ScaleService) {
 		this.form = new FormGroup({
 			cardId: new FormControl("", [Validators.required]),
 			weight: new FormControl("", [Validators.required]),
+		});
+
+		setInterval(() => {
+			this.refreshWeight();
+		}, 1000)
+	}
+
+	private refreshWeight(): void {
+		this.scaleService.getWeight().subscribe((weight) => {
+			this.form.get('weight')?.setValue(weight.lastWeight);
 		});
 	}
 
@@ -28,9 +38,9 @@ export class FoodWeighingComponent {
 				this.utilsService.showSuccessMessage("Self-service cadastrado com sucesso");
 				this.form.reset();
 			},
-      error: () => {
-        this.utilsService.showErrorMessage("Erro ao cadastrar")
-      }
+			error: () => {
+				this.utilsService.showErrorMessage("Erro ao cadastrar");
+			},
 		});
 	}
 }
---
+++ frontend/src/app/services/scale.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface WeightResponse {
+	lastWeight: number;
+}
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ScaleService {
+	constructor(protected readonly http: HttpClient,) {}
+
+	getWeight(): Observable<WeightResponse> {
+    return this.http.get<WeightResponse>('http://192.168.195.170/peso');
+  }
+}
